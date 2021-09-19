d904fadd45e7d88f455bb10c54a8f1bd55fb2366
nori1jz <apriotype2xxnorixx@gmail.com>
2021-09-19 09:45:56 +0000

    [add]新規登録

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,15 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
+    @genres = Genre.all
+
   end
 
   def show
   end
 
   def create
-    genre = Genre.new(genre_params)
+    genre = Genre.new(admin_genre_params)
     genre.save
     redirect_to admin_genres_path
   end
@@ -17,9 +19,9 @@ class Admin::GenresController < ApplicationController
 
   def update
   end
-  
+
   def destroy
-  end  
+  end
 
   private
   def admin_genre_params
